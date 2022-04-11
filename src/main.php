9f30e5da19afbe76dc7f6887cb1c28a28a174d1f
Thue  Vestergaard Iversen <tvi@moonskye.com>
2022-04-11 13:56:53 +0200

    Typo

--- src/main.php
+++ src/main.php
@@ -574,7 +574,7 @@ $discord->on(DiscordEvent::MESSAGE_CREATE, function (Message $message, Discord $
                 }
             }
         }
-        if (!$isCommand && in_array($message->channel_id, $autotranslateChannels)) {
+        if (!$isCommand && in_array($message->channel_id, array_keys($autotranslateChannels))) {
             echo "Found message that need to be autotranslated\n";
             $translations = [];
             foreach ($langs as $lang) {
