71ac7c025440e36d62fe341d1a5c99c47fe6e27e
Thue  Vestergaard Iversen <tvi@moonskye.com>
2022-06-27 12:23:47 +0200

    Fixed some bugs

--- src/chromiumWrapper.php
+++ src/chromiumWrapper.php
@@ -24,8 +24,10 @@ class chromiumWrapper
 
     public function translate($text) {
         $this->page->keyboard()->typeText($text);
+        $this->page->screenshot()->saveToFile('/screenshots/before.png');
         $this->page->waitUntilContainsElement(".J0lOec");
         $outputField = $this->page->evaluate('document.querySelector("#yDmH0d > c-wiz > div > div.WFnNle > c-wiz > div.OlSOob > c-wiz > div.ccvoYb.EjH7wc > div.AxqVh > div.OPPzxe > c-wiz.P6w8m.BDJ8fb > div.dePhmb > div > div.J0lOec").innerText')->getReturnValue();
+        $this->page->screenshot()->saveToFile('/screenshots/after.png');
         return $outputField;
     }
 
